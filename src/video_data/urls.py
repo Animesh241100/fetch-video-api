4492c38b3d6e59bf6d810bc7d8ff77b55af3a7c8
Animesh Kumar <animeshk@pop-os.localdomain>
2022-02-08 11:37:06 +0530

    Add a post API for adding new youtube V3 API Keys

--- src/video_data/urls.py
+++ src/video_data/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     get_video_list,
     search_query,
+    add_api_key,
 )
 
 app_name = 'video_data'
@@ -9,4 +10,5 @@ app_name = 'video_data'
 urlpatterns = [
     path('list/', get_video_list),      # listing the fetched video data
     path('search/', search_query),      # the search query api
+    path('add_v3_key/', add_api_key),      # add a new API key of youtube v3 API
 ]
--- src/video_data/views.py
+++ src/video_data/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -24,7 +25,6 @@ def get_video_list(request):
 # returns the result of searched query in the database as a response
 @api_view(['GET'])
 def search_query(request):
-    print(request.GET)
     title = request.GET.get('title', '')
     desc = request.GET.get('description', '')
     print(f"SEARCH: title: {title}, description : {desc}")
@@ -33,6 +33,30 @@ def search_query(request):
     video_objects = VideoData.objects.filter(title=title, description=desc).order_by('-pub_datetime')
     serializer = VideoDataSerializer(video_objects, many=True)
     return Response(serializer.data)
+
+
+
+# add a new API key with limits not exhausted
+@api_view(['POST'])
+def add_api_key(request):
+    data = JSONParser().parse(request)
+    key = data.get('new_v3_key', '')
+    print(f"ADD V3 KEY: new_v3_key: {key}")
+    if (key ==  ''):
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    try:
+        with open("youtube_api_keys.txt", "r") as fd:
+            api_keys = fd.read().splitlines()
+        print(api_keys)
+        if(key not in api_keys):
+            f = open("youtube_api_keys.txt", 'a')
+            f.write(key + '\n')
+            f.close()
+        return Response(status=status.HTTP_201_CREATED)
+
+    except Exception as Arg:
+        print("Exception in add_api_key: ", Arg)
+        return Response({"exception" : "An Exception Occured"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
