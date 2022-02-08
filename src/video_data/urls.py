1d010666cae3c24f9a1635888146616049f4474a
Animesh Kumar <animeshk@pop-os.localdomain>
2022-02-08 14:53:21 +0530

    Add the templates for web interface

--- src/video_data/urls.py
+++ src/video_data/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('list/', get_video_list),      # listing the fetched video data
     path('search/', search_query),      # the search query api
     path('add_v3_key/', add_api_key),      # add a new API key of youtube v3 API (BONUS)
-    path('optimal_search/', optimal_search_query),   # the optimal search query api (BONUS)
+    path('optimal_search/', optimal_search_query, name='optimal_search'),   # the optimal search query api (BONUS)
 ]
